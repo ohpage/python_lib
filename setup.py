fa8587f160889e189dba26dc9e1f340f213e8af3
ohpage <57546824+ohpage@users.noreply.github.com>
2024-11-12 10:00:54 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+from setuptools import setup
 
+setup(
+  name='ojm_lib',
+  version='0.0.1',
+  url=         ,
+  author='Jongmin Oh',
+  author_email='rocket@nate.com',
+  license='jongmin',
+  packages=['ojm_lib'],
+  zip_safe=False,
+  install_requires=['numpy==1.26.4']
+)
